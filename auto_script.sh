7a055b7554f77579490510f4868196444bb4f81a
bagble <34789997+bagble@users.noreply.github.com>
2022-08-02 13:10:45 +0900

    Update auto_script.sh

--- auto_script.sh
+++ auto_script.sh
@@ -9,11 +9,16 @@ sudo apt update && sudo apt upgrade -y
 # Setup
 mkdir -p /root/server
 # Download purpur latest version
-wget -P /root/server/ https://api.purpurmc.org/v2/purpur/$VERSION/latest/download -O force.jar
+wget https://api.purpurmc.org/v2/purpur/$VERSION/latest/download -O force.jar
 # Download server runner By - monun
-wget -P /root/server/ https://raw.githubusercontent.com/bagble/MDS_vultr/master/run -O run
+wget https://raw.githubusercontent.com/bagble/MDS_vultr/master/run -O run
 # OP
-wget $OP_LIST -p /root/server -O ops.json
+wget $OP_LIST -O ops.json
+
+# Move Files
+mv force.jar /root/server/
+mv run /root/server/
+mv ops.json /root/server/
 
 # Download requirements programs
 sudo apt-get install openjdk-18-jdk -y
